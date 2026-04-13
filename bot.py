9ef5c8c6b36e6137c60f2fdefa908f0234fbb465
Flisti-04 <shymaher2004@gmail.com>
2026-04-14 01:47:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,8 +52,8 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         now = time.time()
 
-        if now < kettle_busy_until:
-    remaining = int(kettle_busy_until - now)
+    if now < kettle_busy_until:
+        remaining = int(kettle_busy_until - now)
 
     await update.message.reply_text(
         f"⛔ СТОП! НЕ ВМИКАТИ!\n"
