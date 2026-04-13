46fa0c7c26061b7d45d765b6c0005dc34e02f3be
Flisti-04 <shymaher2004@gmail.com>
2026-04-14 01:59:51 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -48,23 +48,25 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
     chat_id = update.effective_chat.id
 
-    if text == "☕ Увімкнути чайник":
+if text == "☕ Увімкнути чайник":
 
-        now = time.time()
+    now = time.time()
 
-        if now < kettle_busy_until:
-            remaining = int(kettle_busy_until - now)
+    if now < kettle_busy_until:
+        remaining = int(kettle_busy_until - now)
 
-            await update.message.reply_text(
-                f"⛔ СТОП! НЕ ВМИКАТИ!\n"
-                f"☕ Чайник вже увімкнено\n"
-                f"⏳ {remaining//60}:{remaining%60:02d}"
+        await update.message.reply_text(
+            f"⛔ СТОП! НЕ ВМИКАТИ!\n"
+            f"☕ Чайник вже увімкнено\n"
+            f"⏳ {remaining//60}:{remaining%60:02d}"
         )
         return
 
     kettle_busy_until = now + 7 * 60
 
-    msg = await update.message.reply_text("☕ Чайник увімкнено\n⏳ 7:00")
+    msg = await update.message.reply_text(
+        "☕ Чайник увімкнено\n⏳ 7:00"
+    )
 
     asyncio.create_task(
         countdown_message(context.bot, chat_id, msg.message_id, 7*60)
