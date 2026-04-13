efadf49148a879e03379698a5dcc56b9cd81da2b
Flisti-04 <shymaher2004@gmail.com>
2026-04-14 00:11:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import time
+import asyncio
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, MessageHandler, ContextTypes, filters
 
@@ -14,6 +15,27 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     users.add(update.effective_chat.id)
     await update.message.reply_text("Додано в систему ☕", reply_markup=keyboard)
 
+
+async def countdown_message(bot, chat_id, message_id, seconds):
+    while seconds >= 0:
+        mins = seconds // 60
+        secs = seconds % 60
+
+        text = f"☕ Чайник увімкнено\n⏳ {mins}:{secs:02d}\nНЕ ВМИКАЙ СВІЙ ⚠️"
+
+        try:
+            await bot.edit_message_text(
+                chat_id=chat_id,
+                message_id=message_id,
+                text=text
+            )
+        except:
+            pass
+
+        await asyncio.sleep(1)
+        seconds -= 1
+
+
 async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
     global kettle_busy_until
 
@@ -26,21 +48,29 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         if now < kettle_busy_until:
             remaining = int(kettle_busy_until - now)
-            await update.message.reply_text(f"⚠️ Зайнято! ще {remaining} сек")
+            await update.message.reply_text(f"⚠️ Зайнято ще {remaining//60}:{remaining%60:02d}")
             return
 
-        kettle_busy_until = now + 7 * 60  # 7 хв
+        kettle_busy_until = now + 7 * 60
 
+        # повідомлення з таймером
+        msg = await update.message.reply_text("☕ Чайник увімкнено\n⏳ 7:00\nНЕ ВМИКАЙ СВІЙ ⚠️")
+
+        # запускаємо таймер
+        asyncio.create_task(
+            countdown_message(context.bot, update.effective_chat.id, msg.message_id, 7*60)
+        )
+
+        # повідомляємо всіх
         for user in users:
             try:
                 await context.bot.send_message(
                     chat_id=user,
-                    text="☕ ЧАЙНИК УВІМКНЕНО! НЕ ВМИКАЙ СВІЙ ⚠️"
+                    text="☕ УВАГА! ЧАЙНИК УВІМКНЕНО ⚠️"
                 )
             except:
                 pass
 
-        await update.message.reply_text("Ок, сигнал відправлено 🚨")
 
 app = Application.builder().token(TOKEN).build()
 
